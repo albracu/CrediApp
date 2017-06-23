9c671bed26c7ea71ca8fd2fcdfb7252275e30753
jggomezt <juan.gomez01@gmail.com>
2017-06-23 16:28:33 -0500

    se cambia la forma de acceder al login

--- app/src/main/java/co/alfredobravocuero/crediapp/repository/LoginService.java
+++ app/src/main/java/co/alfredobravocuero/crediapp/repository/LoginService.java
@@ -1,16 +1,23 @@
 package co.alfredobravocuero.crediapp.repository;
 
+import android.os.AsyncTask;
 import android.util.Log;
 
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+
+import co.alfredobravocuero.crediapp.entities.ErrorResponse;
 import co.alfredobravocuero.crediapp.entities.LoginResponse;
-import co.alfredobravocuero.crediapp.entities.ServiceResponse;
-import co.alfredobravocuero.crediapp.repository.service.ILoginService;
 import co.alfredobravocuero.crediapp.repository.service.IResponseService;
 import co.alfredobravocuero.crediapp.repository.service.ITokenResponse;
-import co.alfredobravocuero.crediapp.utilities.Utilities;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import co.alfredobravocuero.crediapp.utilities.Constantes;
 
 /**
  * Created by jggomez on 15-Jun-17.
@@ -29,31 +36,89 @@ public class LoginService {
                 @Override
                 public void getToken(String token) {
 
-                    ILoginService loginService = Utilities.getRetrofitService(ILoginService.class);
+                    new AsyncTask<String, Void, Object>() {
 
-                    Call<ServiceResponse<LoginResponse>> call = loginService.login(user, token);
-
-                    call.enqueue(new Callback<ServiceResponse<LoginResponse>>() {
                         @Override
-                        public void onResponse(Call<ServiceResponse<LoginResponse>> call, Response<ServiceResponse<LoginResponse>> response) {
+                        protected void onPostExecute(Object resp) {
+                            if (resp instanceof LoginResponse) {
+                                loginResponse.response((LoginResponse) resp);
+                            } else if (resp instanceof Exception) {
+                                loginResponse.error((Exception) resp);
+                            } else {
+                                ErrorResponse errorResponse = (ErrorResponse) resp;
+                                loginResponse.error(new Exception(errorResponse.getMessage()));
+                            }
+                        }
 
-                            ServiceResponse<LoginResponse> resultLogin = response.body();
-                            if (response.isSuccessful()) {
-                                if (response.body().isSuccess() && resultLogin.getResult() != null) {
-                                    loginResponse.response(resultLogin.getResult());
-                                    return;
+                        @Override
+                        protected Object doInBackground(String... params) {
+
+                            String accessKey = params[0];
+                            String username = params[1];
+
+                            String data = null;
+                            BufferedReader reader = null;
+
+                            try {
+
+                                data = URLEncoder.encode("username", "UTF-8")
+                                        + "=" + URLEncoder.encode(username, "UTF-8");
+                                data += "&" + URLEncoder.encode("accessKey", "UTF-8") + "="
+                                        + URLEncoder.encode(accessKey, "UTF-8");
+
+                                // Defined URL  where to send data
+                                URL url = new URL(Constantes.URL_BASE + Constantes.OPERATION_LOGIN);
+
+                                // Send POST data request
+                                URLConnection conn = url.openConnection();
+                                conn.setDoOutput(true);
+                                OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+                                wr.write(data);
+                                wr.flush();
+
+                                // Get the server response
+                                reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                                StringBuilder sb = new StringBuilder();
+                                String line = null;
+
+                                // Read Server Response
+                                while ((line = reader.readLine()) != null) {
+                                    // Append server response in string
+                                    sb.append(line + "\n");
+                                }
+
+                                JSONObject resp = (JSONObject) new JSONTokener(sb.toString()).nextValue();
+
+                                boolean success = (Boolean) resp.get("success");
+
+                                if (success) {
+                                    JSONObject result = resp.getJSONObject("result");
+                                    LoginResponse objLoginResponse = new LoginResponse();
+                                    objLoginResponse.setSessionName(result.getString("sessionName"));
+                                    objLoginResponse.setUserId(result.getString("userId"));
+                                    objLoginResponse.setVersion(result.getString("version"));
+                                    objLoginResponse.setVtigerVersion(result.getString("vtigerVersion"));
+                                    return objLoginResponse;
+                                } else {
+                                    JSONObject error = resp.getJSONObject("error");
+                                    ErrorResponse errorResponse = new ErrorResponse();
+                                    errorResponse.setCode(error.getString("code"));
+                                    errorResponse.setMessage(error.getString("message"));
+                                    return errorResponse;
+                                }
+
+                            } catch (Exception ex) {
+                                return ex;
+                            } finally {
+                                try {
+                                    reader.close();
+                                } catch (Exception ex) {
+                                    return ex;
                                 }
                             }
 
-                            loginResponse.error(new Exception(resultLogin.getError().getMessage()));
-
                         }
-
-                        @Override
-                        public void onFailure(Call<ServiceResponse<LoginResponse>> call, Throwable t) {
-                            loginResponse.error(new Exception(t));
-                        }
-                    });
+                    }.execute(token, user);
                 }
             });
 
--- app/src/main/java/co/alfredobravocuero/crediapp/utilities/Constantes.java
+++ app/src/main/java/co/alfredobravocuero/crediapp/utilities/Constantes.java
@@ -9,4 +9,6 @@ public class Constantes {
     public static final String URL_BASE = "https://crm.novation.com.co/";
 
     public static final String ACCESS_KEY = "ZvPZIFf06TGKchK";
+
+    public static final String OPERATION_LOGIN = "webservice.php?operation=login";
 }
